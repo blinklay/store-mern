0ea4b3128b2a2ac8992fbb328984516483c572ae
blinklay <danillitvinenkod00@gmail.com>
2025-04-16 14:25:20 +0300

    install concurrently & add cors settings to server

--- server/index.js
+++ server/index.js
@@ -5,16 +5,22 @@ const cookieParser = require("cookie-parser");
 const adminRouter = require("./routes/adminRoutes");
 const productRouter = require("./routes/productRoutes");
 const userRouter = require("./routes/userRoutes");
+const cors = require("cors")
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log("DB start!"))
   .catch(e => console.log("DB crash!", e))
 
 const app = express()
+app.use(cors({
+  origin: CLIENT_ORIGIN,
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json())
 
